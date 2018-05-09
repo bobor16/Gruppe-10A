1f5619de9151b82f90e922339ab09264a694ad75
Borgar Bordoy <bobor16@student.sdu.dk>
2018-05-09 09:40:36 +0200

    import file

--- JavaApplication26/src/gse01/GSE01.java
+++ JavaApplication26/src/gse01/GSE01.java
@@ -1,14 +1,16 @@
 package gse01;
 
-import javaapplication26.gseLogic;
+import gse01.gseLogic;
 
 public class GSE01 {
+
     public static void main(String[] args) {
-       GSE01 platform1 = new GSE01();
-       platform1.init();
+        GSE01 platform1 = new GSE01();
+        platform1.init();
     }
-    public void init(){
-       gseLogic logic = new gseLogic(); 
-       logic.start();
+
+    public void init() {
+        gseLogic logic = new gseLogic();
+        logic.start();
     }
 }
